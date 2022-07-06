ef93846344eed18f819c8b2df17fa19037b77fa8
Reibnitz <lucas.fpolis@gmail.com>
2022-07-06 14:53:36 -0300

    create: enum

---
+++ Sem_6_Ex_4/Enum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem_6_Ex_4
+{
+    public enum Opcoes
+    {
+        ListarProdutos = 1,
+        ListarCarrinho,
+        AdicionarAoCarrinho,
+        RemoverDoCarrinho
+    }
+}
