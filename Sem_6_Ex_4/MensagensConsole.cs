3f5b2fad56714841ba5f697eeec601f90d6fbae6
Reibnitz <lucas.fpolis@gmail.com>
2022-07-07 16:26:41 -0300

    Remove: namespaces inúteis

--- Sem_6_Ex_4/MensagensConsole.cs
+++ Sem_6_Ex_4/MensagensConsole.cs
@@ -1,10 +1,5 @@
 ﻿using Sem_6_Ex_4.Exceptions;
 using Sem_6_Ex_4.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Sem_6_Ex_4
 {
