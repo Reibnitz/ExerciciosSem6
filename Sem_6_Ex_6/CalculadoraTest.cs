287833945ce036e0ca5a118d73ede4ba4cdada75
Reibnitz <lucas.fpolis@gmail.com>
2022-07-08 13:56:03 -0300

    create: Exercício 6

---
+++ Sem_6_Ex_6/CalculadoraTest.cs
@@ -0,0 +1,61 @@
+﻿using Sem_6_Ex_2;
+using Sem_6_Ex_2.Exceptions;
+using Xunit;
+
+namespace Sem_6_Ex_6
+{
+    public class CalculadoraTest
+    {
+        [Fact]
+        public void Somar_DeveriaSomar()
+        {
+            // Arrange
+            double esperado = 5;
+
+            // Act
+            double obtido = Calculadora.Somar(2, 3);
+
+            // Assert
+
+            Assert.Equal(esperado, obtido);
+        }
+
+        [Fact]
+        public void Subtrair_DeveriaSubtrair()
+        {
+            double esperado = 1;
+
+            double obtido = Calculadora.Subtrair(3, 2);
+
+            Assert.Equal(esperado, obtido);
+        }
+
+        [Fact]
+        public void Multiplicar_DeveriaMultiplicar()
+        {
+            double esperado = 3;
+
+            double obtido = Calculadora.Multiplicar(2, 1.5);
+
+            Assert.Equal(esperado, obtido);
+        }
+
+        [Fact]
+        public void Dividir_DeveriaDividir()
+        {
+            double esperado = 1.5;
+
+            double obtido = Calculadora.Dividir(3, 2);
+
+            Assert.Equal(esperado, obtido);
+        }
+
+        [Fact]
+        public void Dividir_NaoDeveriaDividirPorZero()
+        {
+            Assert.Throws<CalculoInvalidoException>(
+                () => Calculadora.Dividir(1, 0)
+            );
+        }
+    }
+}
---
+++ Sem_6_Ex_6/MensagensConsoleTest.cs
@@ -0,0 +1,64 @@
+﻿using Sem_6_Ex_2;
+using Sem_6_Ex_2.Exceptions;
+using Xunit;
+
+
+namespace Sem_6_Ex_6
+{
+    public class MensagensConsoleTest
+    {
+        [Fact]
+        public void ProcessarSelecaoDeOperacao_DeveriaProcessarComNumeroValido()
+        {
+            int esperado = 1;
+
+            int obtido = MensagensConsole.ProcessarSelecaoDeOperacao("1");
+
+            Assert.Equal(esperado, obtido);
+        }
+
+        [Fact]
+        public void ProcessarSelecaoDeOperacao_NaoDeveriaProcessarNumeroInvalido()
+        {
+            Assert.Throws<OpcaoNaoExistenteException>(
+                () => MensagensConsole.ProcessarSelecaoDeOperacao("5")
+            );
+        }
+
+        [Fact]
+        public void ProcessarSelecaoDeOperacao_NaoDeveriaProcessarTexto()
+        {
+            Assert.Throws<EntradaInvalidaException>(
+                () => MensagensConsole.ProcessarSelecaoDeOperacao("texto qualquer")
+            );
+        }
+
+        [Theory]
+        [InlineData("5.5")]
+        [InlineData("5,5")]
+        public void ProcessarSelecaoDeValor_DeveriaProcessarNumerosComPontoOuVirgula(string inputUsuario)
+        {
+            double esperado = 5.5;
+            double obtido = MensagensConsole.ProcessarSelecaoDeValor(inputUsuario);
+
+            Assert.Equal(esperado, obtido);
+        }
+
+        [Fact]
+        public void ProcessarSelecaoDeValor_DeveriaProcessarNumerosInteiros()
+        {
+            double esperado = 10;
+            double obtido = MensagensConsole.ProcessarSelecaoDeValor("10");
+
+            Assert.Equal(esperado, obtido);
+        }
+
+        [Fact]
+        public void ProcessarSelecaoDeValor_NaoDeveriaProcessarTexto()
+        {
+            Assert.Throws<EntradaInvalidaException>(
+                () => MensagensConsole.ProcessarSelecaoDeValor("texto qualquer")
+            );
+        }
+    }
+}
